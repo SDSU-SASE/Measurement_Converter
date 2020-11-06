edd5a9d89112ea979d9235b44857f4830fc8b37b
Cornell T <trancornellh@gmail.com>
2020-11-05 16:14:07 -0800

    added main.c and metricToImperial.c

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+ *  main.c
+ *
+ *  Edited:    November 5, 2020
+ *  Author(s): Cornell
+ */
+ 
+ #include <stdio.h>
+ 
+ int main (void)
+ {
+	 float testMeters;
+	 printf("This is to test metricToImperial.c");
+	 printf("Enter how many meters: ");
+	 scanf("%f", &testMeters);
+ }
---
+++ metricToImperial.c
@@ -0,0 +1,14 @@
+/*
+ *  metricToImperial.c
+ *
+ *  Edited:    November 5, 2020
+ *  Author(s): Cornell
+ */
+ 
+ #include <stdio.h>
+ 
+ float convert(float metric)
+ {
+	 float imperial;
+	 return imperial;
+ }
